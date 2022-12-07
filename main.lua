cf1ef9d54a99deabb4d53fdf673a906fe0e612b6
Grunghiv <61913098+Grunghiv@users.noreply.github.com>
2022-12-07 00:14:38 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,18 @@
 require "card"
+require "mouse_functions"
 
 function love.load()
-    NewCard = Card:Initialize('c', '4')
+    love.window.setMode(1280, 720, {vsync = true})
+    NewCard = Card:new(Card, 'c', '4')
 end
 
 function love.update(dt)
-
+    if NewCard.dragging.active then
+        NewCard.x = love.mouse.getX() - NewCard.dragging.diffX
+        NewCard.y = love.mouse.getY() - NewCard.dragging.diffY
+    end
 end
 
 function love.draw()
-    love.graphics.draw(NewCard, 0, 0)
+    
 end
