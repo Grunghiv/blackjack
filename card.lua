a681a1da304d5a21b91c300c947962ee8717c9a2
Grunghiv <61913098+Grunghiv@users.noreply.github.com>
2022-12-07 00:00:13 -0600

    Update card.lua

--- card.lua
+++ card.lua
@@ -1,18 +1,34 @@
-function Card:initialize(suit, value)
-    Card = {}
+Card = {suit = '', value = ''}
+
+function Card:new(o, suit, value)
+    Card = o or {}
+    setmetatable(Card, self)
+    self.__index = self
     self.suit = suit
     self.value = value
     self.image = love.graphics.newImage('assets/cards/'..suit..value..'.png')
-    self.__index = self
+    self.x = 10
+    self.y = 10
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+    self.dragging = {active = false, diffX = 0, diffY = 0}
+    return Card
 
 end
 
-function Card:setSuit(suit)
-    self.suit = suit
+function Card:update(o)
+    o.image = love.graphics.newImage('assets/cards/'..o.suit..o.value..'.png')
+    return o
+end
+
+function Card:setSuit(o, suit)
+    o.suit = suit
+    return o
 
 end
 
-function Card:setValue(value)
-    self.value = value
+function Card:setValue(o, value)
+    o.value = value
+    return o
 
 end
